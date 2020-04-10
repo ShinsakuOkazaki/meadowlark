d40f9bad5abfcb98c530f5e166d840766627f23d
sinsakuokazaki <golisaku4639@gmail.com>
2020-04-09 20:52:37 -0400

    cookies and session chapter9

--- meadowlark.js
+++ meadowlark.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
-
+var formidable = require('formidable');
+//var jqupload = require('jquery-file-upload-middleware');
 var app = express();
 
 //var credentials = require('./credentials.js');
 
-// set up handlebars view engine
+// set up handlebars view engine for server-side templating and caching template.
 var handlebars = require('express3-handlebars').create({
     defaultLayout:'main',
     helpers: {
@@ -23,6 +24,7 @@ app.set('port', process.env.PORT || 3000);
 // static middleware: it is equivarent to creating a route for each static file you want to 
 // deliver that renders a file and returns it to the client.
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 
 app.use(function(req,res, next) {
 	res.locals.showTests = app.get('env') != 'production' && req.query.test === '1';
@@ -37,6 +39,53 @@ app.get('/about', function(req,res){
 						  pageTestScript: '/qa/tests-about.js'});
 });
 
+
+app.get('/newsletter', function(req, res){
+	res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+
+app.post('/process', function(req, res){
+	if(req.xhr || req.accepts('json, html') === 'json'){
+		res.send({success: true});
+	} else {
+		res.redirect(303, '/thank-you');
+	}
+});
+
+app.get('/contest/vacation-photo', function(req, res){
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(), 
+		month: now.getMonth()
+	});
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files){
+		if(err) return res.redirect(303, '/error');
+
+		console.log('recieved fields:');
+		console.log(fields);
+		console.log('recieved files:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
+});
+
+
+// app.use('/upload', function(req, res, next){
+// 	var now = Date.now();
+// 	jqupload.fileHandler({
+// 		uploadDir: function(){
+// 			return __dirname + '/public/uploads/' + now;
+// 		}, 
+// 		uploadUrl: function() {
+// 			return '/uploads/' + now;
+// 		}, 
+// 	})(req, res, next);
+// }) ;
+
 app.get('/tours/hood-river', function(req, res) {
 	res.render('tours/hood-river');
 });
@@ -47,6 +96,19 @@ app.get('/tours/oregon-coast', function(req, res) {
 
 app.get('/tours/request-group-rate', function(req, res) {
 	res.render('tours/request-group-rate');
+});
+
+app.get('/nursery-rhyme', function(req, res) {
+	res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme', function(req, res) {
+	res.json({
+		animal: 'squirrel', 
+		bodyPart: 'tail', 
+		adjective: 'bushy', 
+		noun: 'heck',
+	});
 });
 
 // 404 catch-all handler (middleware)
